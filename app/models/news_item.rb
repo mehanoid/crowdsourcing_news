ab51a81d1492e3272de5c10859fcdbb92ca72e6f
Oleg Grigoriev <mehanoid.ru@gmail.com>
2014-07-13 15:48:01 +0400

    Order news by created_at

--- app/models/news_item.rb
+++ app/models/news_item.rb
@@ -3,4 +3,6 @@ class NewsItem < ActiveRecord::Base
   has_and_belongs_to_many :categories, class_name: 'NewsCategory'
   validates :title, :description, :body, :user, presence: true
   validates :categories, length: { minimum: 1, message: 'should have at least one category' }
+
+  default_scope { order created_at: :desc }
 end
